82ee49e54256ece6fa7a7802bf6eb8f9fc79435b
Ayoub Rebhi <74020228+AyoubRebhi@users.noreply.github.com>
2024-02-21 17:21:31 +0100

    tableView ==> ListView :)

--- src/main/java/tn/esprit/Controllers/AfficherCategories.java
+++ src/main/java/tn/esprit/Controllers/AfficherCategories.java
@@ -5,11 +5,13 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -18,6 +20,7 @@ import tn.esprit.Models.Projet;
 import tn.esprit.Services.CategorieServices;
 import tn.esprit.test.HelloApplication;
 import javafx.scene.Scene;
+import java.util.Map;
 
 
 public class AfficherCategories {
@@ -29,30 +32,25 @@ public class AfficherCategories {
     private URL location;
 
     @FXML
-    private TableColumn<Categorie, Integer> idCol;
-
-    @FXML
-    private TableColumn<Categorie, Integer> nbProjetsCol;
-
-    @FXML
-    private TableView<Categorie> tableView;
-
-    @FXML
-    private TableColumn<Categorie, String> titreCol;
+    private ListView<String> listView;
 
     @FXML
     void initialize() {
         CategorieServices categorieServices = new CategorieServices();
         try {
-            List<Categorie> categories =  categorieServices.afficher();
-            //observableList ==> Run f real time
-            ObservableList<Categorie> observableList = FXCollections.observableList(categories);
-            tableView.setItems(observableList);
-
-            idCol.setCellValueFactory(new PropertyValueFactory<>("id_categorie"));
-            titreCol.setCellValueFactory(new PropertyValueFactory<>("titre"));
-
-
+            List<Categorie> categories = categorieServices.afficher();
+            ObservableList<String> observableList = FXCollections.observableArrayList();
+            String idName="ID,     ";
+            String categorieName="Categorie,     ";
+            String nbProjetName="Nombre de projets, ";
+            observableList.add(idName+categorieName+nbProjetName);
+            for (Categorie categorie : categories) {
+                int idCategorie = categorie.getId_categorie();
+                String titreCategorie = categorie.getTitre();
+                int nbProjets = categorieServices.calculerNbProjets(idCategorie).get(titreCategorie);
+                observableList.add(idCategorie+",       "+titreCategorie +",                "+ nbProjets);
+            }
+            listView.setItems(observableList);
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
