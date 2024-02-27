3fdc5a111e3e0b488151b358b486a1bb321fa013
Ayoub Rebhi <74020228+AyoubRebhi@users.noreply.github.com>
2024-02-27 20:46:13 +0100

    fixing design

--- src/main/java/tn/esprit/test/HelloApplication.java
+++ src/main/java/tn/esprit/test/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/sidebarAdmin.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/sidebar.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Projets");
         stage.setScene(scene);
